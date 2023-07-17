13b0307c730473c69a8cf752b2e81c44e4cff11a
“ProfessionalDevOp” <emangabikoue@yahoo.fr>
2023-07-16 22:22:20 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "team" {
+    default = "dev"
+}
+
+variable "keypair" {
+    default = "terraform-key1"
+}
+
+variable "vpc_name" {
+    default = "utc-app1" 
+}
